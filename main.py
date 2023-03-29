0df11c87f377e484254ae950b70922c16d421b1e
unknown <guslindell2@gmail.com>
2023-03-28 19:52:45 -0500

    Pygame boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+import pygame
+import random as rand
+
 print("Hello World!")
+
+def random_colorint():
+    return rand.randrange(0, 255, 1)
+
+def random_color():
+    return pygame.Color(random_colorint(), random_colorint(), random_colorint(), 255)
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+
+running = True
+render = True
+
+screencolor = random_color()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            screencolor = random_color()
+            render = True
+    
+    if render == True:
+        screen.fill(screencolor)
+
+        pygame.display.flip()
+        render = False
+
+    pass
+
+print("End")
+pygame.quit()
