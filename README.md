c683c0f40ff1497e7a208f817110d6fe276d7a3c
LeanHydra84 <guslindell2@gmail.com>
2023-03-28 20:21:09 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,12 @@
+**README**
+
+1. To run this program you must first have Python 3 installed
+2. To run this program you must also have pygame installed
+
+3. If you're using Visual Studio Code and you're getting yellow squigglies around 'import pygame':
+    >Press Control+Shift+P
+    >Search for python select interpreter
+    >Select "Python: Select Interpreter"
+    >Choose the version of python you installed
+This will change the interpreter to the python you have installed and not the one packaged with
+the VSCode Python package.
--- main.py
+++ main.py
@@ -18,20 +18,32 @@ render = True
 screencolor = random_color()
 
 while running:
+
+    # Check for events
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+
+        if event.type == pygame.QUIT: # On Screen Close
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+
+        if event.type == pygame.MOUSEBUTTONDOWN: # On Mouse Down
             screencolor = random_color()
             render = True
+
+        if event.type == pygame.KEYDOWN: # On Key Down
+
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
     
+    # Draw Screen
     if render == True:
+
+        # Begin draw frame
         screen.fill(screencolor)
 
+
+        # End of frame render
         pygame.display.flip()
         render = False
 
-    pass
-
-print("End")
 pygame.quit()
